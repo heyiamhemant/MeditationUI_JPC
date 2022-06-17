75a8b5c7924363b454f17f2bf4136ad6326cb076
Hemant Goswami <goswamihemant3@gmail.com>
2022-06-17 22:15:49 +0530

    Initial commit.

---
+++ app/src/main/java/com/plcoding/meditationui_jpc/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.plcoding.meditationui_jpc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.meditationui_jpc.ui.HomeScreen
+import com.plcoding.meditationui_jpc.ui.theme.MeditationUIYouTubeTheme
+
+class MainActivity : ComponentActivity() {
+    @ExperimentalFoundationApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MeditationUIYouTubeTheme {
+                HomeScreen()
+            }
+        }
+    }
+}
